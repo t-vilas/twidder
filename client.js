d8898a549f41adbd507cde5a60c17e93c49e5405
Tony VILAS <tony.vilas.contact@gmail.com>
2015-01-27 13:01:42 +0100

    Adding all the necessary files for lab 1. Part 5, Step 0 : Completed.

---
+++ client.js
@@ -0,0 +1,11 @@
+displayView = function()
+{
+	// The code required to display a view.
+};
+
+window.onload = function()
+{
+	// code that is executed as the page is loaded.
+	// You shall put your own custom code here.
+	window.alert("Hello TDDD97 !");
+};
