4ead015432003e8fdf8a50d03ff9df95fad409d0
Livaï <dyavil.q@gmail.com>
2015-01-29 12:01:28 +0100

    js signin_out : step 4

--- client.js
+++ client.js
@@ -1,13 +1,24 @@
+
+
 displayView = function()
 {
 	// The code required to display a view.
+	var tok = JSON.parse(localStorage.getItem("token"));
+	var div = document.getElementById('nonlogview');
+	var elem = document.getElementById('notlogview');
+	if (tok != null){
+		elem = document.getElementById('logview');
+	}
+	div.innerHTML = elem.innerHTML;
 };
 
 window.onload = function()
 {
-	var elem = document.getElementById('notlogview');
+
+	displayView();
+	/*var elem = document.getElementById('notlogview');
 	var div = document.getElementById('nonlogview');
-	div.innerHTML = elem.innerHTML;
+	div.innerHTML = elem.innerHTML;*/
 };
 
 function signup(){
@@ -25,6 +36,12 @@ function signup(){
 		return false;
 	}
 
+	var dataform = {email: document.getElementById('email').value, password: document.getElementById('password').value, firstname: document.getElementById('firstname').value, familyname: document.getElementById('familyname').value, gender: document.getElementById('gender').value, city: document.getElementById('city').value, country: document.getElementById('country').value};
+
+	var res = serverstub.signUp(dataform);
+
+	window.alert(res.message);
+
 
 };
 
@@ -36,4 +53,24 @@ function signin(){
 		return false;
 	}
 
-}
+	var res = serverstub.signIn(document.getElementById('siemail').value, pass);
+	var token = null;
+	if (res.success == true){
+	token = res.data;
+	}
+	localStorage.setItem("token", JSON.stringify(token));
+	displayView();
+	window.alert(res.message);
+	return true;
+
+};
+
+function logout(){
+	var tok = JSON.parse(localStorage.getItem("token"));
+	localStorage.setItem("token", null);
+	window.alert('youpi3');
+	var res = serverstub.signOut(tok);
+
+	displayView();
+	return true;
+};
