3f17a9fd261a634f61ff7c483dccf29e77b11040
Livaï <dyavil.q@gmail.com>
2015-01-29 10:59:27 +0100

    js password check : step 3

--- client.js
+++ client.js
@@ -8,5 +8,32 @@ window.onload = function()
 	var elem = document.getElementById('notlogview');
 	var div = document.getElementById('nonlogview');
 	div.innerHTML = elem.innerHTML;
-	window.alert("Hello TDDD97 !");
 };
+
+function signup(){
+	
+	var pass1 = document.getElementById('password').value;
+	var pass2 = document.getElementById('repeat_password').value;
+
+	if (pass1 != pass2){
+		window.alert('different passwords');
+		return false;
+	}
+
+	if (pass1.length < 8){
+		window.alert('password too short (min 8)');
+		return false;
+	}
+
+
+};
+
+function signin(){
+	var pass = document.getElementById('sipassword').value;
+
+	if (pass.length < 8){
+		window.alert('password too short (min 8)');
+		return false;
+	}
+
+}
